c5ea314976633aeaecba872544118a591205414c
EDUARDO NASCIMENTO DE SOUZA ROLIM <eduardo.rolim@ifpr.edu.br>
2023-08-22 17:08:30 -0300

    Correção de texto

--- 02-hello-fastapi/hello_fastapi/main.py
+++ 02-hello-fastapi/hello_fastapi/main.py
@@ -8,17 +8,17 @@ app = FastAPI()
 
 @app.get("/")
 def rota_root():
-    """Hello World na rota /"""
+    """Hello World na rota /."""
     return {"mensagem": "Hello, FastAPI!"}
 
 
 @app.get("/users/me")
 def le_meu_usuario():
-    """Retorna o usuário corrente"""
+    """Retorna o usuário corrente."""
     return {"user_id": "vndmtrx"}
 
 
 @app.get("/users/{user_id}")
 def le_usuario(user_id: str):
-    """Retorna o usuário informado"""
+    """Retorna o usuário informado."""
     return {"user_id": user_id}
