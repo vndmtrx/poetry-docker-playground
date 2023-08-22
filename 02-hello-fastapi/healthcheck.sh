9a9f6227fdd05c0a2ed3f334eb59d9609b1479e4
Eduardo Rolim <vndmtrx@duck.com>
2023-08-22 18:57:47 -0300

    Ajustes no script de healt check.

--- 02-hello-fastapi/healthcheck.sh
+++ 02-hello-fastapi/healthcheck.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-set -o pipefail
+set -euo pipefail
 
 HOST=$1
 PORT=$2
 
-exec 9<>/dev/tcp/"${HOST}"/"${PORT}"
-echo -e "GET /health HTTP/1.1\r\nConnection: close\r\n\r\n" >&9
-grep -q "HTTP/1.1 200 OK" <&9
+exec 9<>"/dev/tcp/${HOST}/${PORT}"
+echo -e "HEAD /health HTTP/1.1\r\nConnection: close\r\n\r\n" >&9
+grep -q "200 OK" <&9
 ret_code=$?
 
 exec 9<&-
