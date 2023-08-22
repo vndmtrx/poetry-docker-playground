0e39e6f5198efb4828134183aef944eb2da5dae5
Eduardo Rolim <vndmtrx@duck.com>
2023-08-22 20:46:21 -0300

    Removida a geração de log do endpoint /health

--- 02-hello-fastapi/hello_fastapi/healthcheck.py
+++ 02-hello-fastapi/hello_fastapi/healthcheck.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python
-"""Módulo de exemplo de FastAPI."""
+"""Módulo de checagem de saúde do projeto."""
 
+import logging
 from fastapi import APIRouter, status
 from pydantic import BaseModel
 
 health = APIRouter()
 
-## Health Checks
-
 
 class HealthCheck(BaseModel):
-    """Model para retorno do health check."""
+    """Model para retorno da checagem de saúde."""
 
     status: str = "OK"
+
+
+class EndpointFilter(logging.Filter):
+    """Filtro para remoção dos logs de saúde dos logs do endpoint."""
+
+    def filter(self, record: logging.LogRecord) -> bool:  # pragma: no cover
+        """Função que retorna se o endpoint é o /health."""
+        return record.args and len(record.args) > 3 and record.args[2] != "/health"
 
 
 @health.get(
@@ -30,4 +37,5 @@ def get_health() -> HealthCheck:  # pragma: no cover
 
 def add_healthcheck(app):  # pragma: no cover
     """Adiciona o router de saúde na API principal."""
+    logging.getLogger("uvicorn.access").addFilter(EndpointFilter())
     app.include_router(health)
